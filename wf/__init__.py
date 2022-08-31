6c1d5832b4420444e03044f862d8c9f235665b71
Kenny Workman <kennyworkman@sbcglobal.net>
2022-08-31 16:05:58 -0700

    feat: use verified reference

--- wf/__init__.py
+++ wf/__init__.py
@@ -19,9 +19,10 @@ from flytekit.core.annotation import FlyteAnnotation
 from flytekitplugins.pod import Pod
 from kubernetes.client.models import (V1Container, V1PodSpec,
                                       V1ResourceRequirements, V1Toleration)
-from latch import map_task, message, small_task, workflow, workflow_reference
+from latch import map_task, message, small_task, workflow
 from latch.resources.launch_plan import LaunchPlan
 from latch.types import LatchDir, LatchFile, file_glob
+from latch.verified import deseq2_wf
 
 
 def _capture_output(command: List[str]) -> Tuple[int, str]:
@@ -694,71 +695,6 @@ def count_matrix_and_multiqc(
 class AlignmentTools(Enum):
     star_salmon = "Traditional Alignment + Quantification"
     salmon = "Selective Alignment + Quantification"
-
-
-@workflow_reference(
-    name="wf.__init__.deseq2_wf",
-    version="1.3.18-7a4e16",
-)
-def deseq2_wf(
-    report_name: str,
-    count_table_source: str = "single",
-    raw_count_table: Optional[
-        Annotated[
-            LatchFile,
-            FlyteAnnotation(
-                {
-                    "_tmp_hack_deseq2": "counts_table",
-                    "rules": [
-                        {
-                            "regex": r".*\.(csv|tsv|xlsx)$",
-                            "message": "Expected a CSV, TSV, or XLSX file",
-                        }
-                    ],
-                }
-            ),
-        ]
-    ] = None,
-    raw_count_tables: List[LatchFile] = [],
-    count_table_gene_id_column: str = "gene_id",
-    output_location_type: str = "default",
-    output_location: Optional[LatchDir] = None,
-    conditions_source: str = "manual",
-    manual_conditions: Annotated[
-        List[List[str]],
-        FlyteAnnotation({"_tmp_hack_deseq2": "manual_design_matrix"}),
-    ] = [],
-    conditions_table: Optional[
-        Annotated[
-            LatchFile,
-            FlyteAnnotation(
-                {
-                    "_tmp_hack_deseq2": "design_matrix",
-                    "rules": [
-                        {
-                            "regex": r".*\.(csv|tsv|xlsx)$",
-                            "message": "Expected a CSV, TSV, or XLSX file",
-                        }
-                    ],
-                }
-            ),
-        ]
-    ] = None,
-    design_matrix_sample_id_column: Optional[
-        Annotated[str, FlyteAnnotation({"_tmp_hack_deseq2": "design_id_column"})]
-    ] = None,
-    design_formula: Annotated[
-        List[List[str]],
-        FlyteAnnotation(
-            {
-                "_tmp_hack_deseq2": "design_formula",
-                "_tmp_hack_deseq2_allow_clustering": True,
-            }
-        ),
-    ] = [],
-    number_of_genes_to_plot: int = 30,
-) -> LatchDir:
-    ...
 
 
 @workflow
