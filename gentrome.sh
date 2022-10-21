eebacadd3a3029b26fab0e40a51b5d4fe18b23d9
Kenny Workman <kennyworkman@sbcglobal.net>
2022-10-20 21:59:25 -0700

    fix: general restructure

--- gentrome.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-genome_ref=$1
-transcript_ref=$2
-
-grep "^>" $genome_ref | cut -d " " -f 1 > decoys.txt
-sed -i.bak -e 's/>//g' /root/decoys.txt
-cat $transcript_ref $genome_ref  > /root/gentrome.fa
